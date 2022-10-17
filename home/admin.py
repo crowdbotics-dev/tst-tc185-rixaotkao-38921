d30f4a4c32c22999031ef54539d7beb33ae1ede4
Crowdbotics <engineering@crowdbotics.com>
2022-10-17 00:36:33 +0000

    Update models

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Addressjjpzxiponv
+
+admin.site.register(Addressjjpzxiponv)
 
 # Register your models here.
--- tst_tc185_rixaotkao_38921/wsgi.py
+++ tst_tc185_rixaotkao_38921/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tst_tc185_rixaotkao_38921.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tst_tc185_rixaotkao_38921.settings")
 
 application = get_wsgi_application()
